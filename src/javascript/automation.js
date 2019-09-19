5200bb6e8e92d463bde978cf020ee4d3dc933e77
Mohcka <bandagedwolf95@gmail.com>
2019-09-19 14:05:46 -0700

    added query input

--- src/javascript/automation.js
+++ src/javascript/automation.js
@@ -7,7 +7,9 @@ const geckoPath = require('geckodriver').path
 
 const Swal2 = require('sweetalert2')
 
-exports.runAutomation = async phrases => {
+const { chunkify } = require('../utils/array_utils')
+
+exports.runAutomation = async (phrases, instances = 1) => {
   // Set service builder to use geckodriver installed in node_modules
   await new firefox.ServiceBuilder(geckoPath).build()
   // init FFox options
@@ -16,31 +18,86 @@ exports.runAutomation = async phrases => {
   fireOpts.setPreference('dom.webnotifications.enabled', false)
   fireOpts.setPreference('dom.push.enabled', false)
 
-  try {
-    // Create Driver with chosen browser and configs
-    driver = await new Builder()
+  // Create n number of drivers for every instance requested
+  let phrasesChunks = chunkify(phrases, instances).map(phrasesChunk => {
+    // Instanciate a new driver for this chunk
+    let driverInstance = new Builder()
       .forBrowser('firefox')
       .withCapabilities(fireOpts)
-      .build()
-    // Try a google search
-    await driver.get('http://www.google.com/ncr')
-    await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
-    await driver.wait(until.titleIs('webdriver - Google Search'), 1000)
 
-    Swal2.fire({
-      type: 'success',
-      title: 'Automation Complete',
-      heightAuto: false,
+    return { phrases: phrasesChunk, driverInstance: driverInstance }
+  })
+
+  console.log(phrasesChunks)
+
+  try {
+    for (let i = 0; i < phrasesChunks.length; i++) {
+      phrasesChunks[i].driverInstance = await phrasesChunks[
+        i
+      ].driverInstance.build()
+    }
+    console.log('before map')
+
+    phrasesChunks.forEach(async phraseChunk => {
+      console.log('After map')
+      let driverInstance = phraseChunk.driverInstance
+
+      for (let j = 0; j < phraseChunk.phrases.length; j++) {
+        let searchPhrase = phraseChunk.phrases[j]
+        console.log(searchPhrase)
+
+        await driverInstance.get('http://www.google.com/ncr')
+        await driverInstance
+          .findElement(By.name('q'))
+          .sendKeys(searchPhrase, Key.RETURN)
+        await driverInstance.wait(
+          until.titleIs(`${searchPhrase} - Google Search`),
+          5000
+        )
+        // open new tab
+        // console.log(`${j + 1} : ${phraseChunk.phrases.length}`)
+        if (j + 1 < phraseChunk.phrases.length) {
+          await driverInstance.executeScript("window.open('','_blank');")
+          let handles
+          await driverInstance.getAllWindowHandles().then(h => (handles = h))
+          console.log(handles)
+
+          await driverInstance.switchTo().window(handles[handles.length - 1])
+          // await driverInstance.wait(until.titleIs(`New Tab`), 5000)
+        }
+      }
     })
   } catch (err) {
-    console.log(err)
-
-    Swal2.fire({
-      title: 'Error',
-      text:
-        'An error occured running the automation.  Perhaps something went wrong with the installation',
-      type: 'error',
-      heightAuto: false,
-    })
+    console.error(err)
+  } finally {
+    console.log('Done')
   }
+
+  // try {
+  //   // Create Driver with chosen browser and configs
+  //   driver = await new Builder()
+  //     .forBrowser('firefox')
+  //     .withCapabilities(fireOpts)
+  //     .build()
+  //   // Try a google search
+  //   await driver.get('http://www.google.com/ncr')
+  //   await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN)
+  //   await driver.wait(until.titleIs('webdriver - Google Search'), 1000)
+
+  //   Swal2.fire({
+  //     type: 'success',
+  //     title: 'Automation Complete',
+  //     heightAuto: false,
+  //   })
+  // } catch (err) {
+  //   console.log(err)
+
+  //   Swal2.fire({
+  //     title: 'Error',
+  //     text:
+  //       'An error occured running the automation.  Perhaps something went wrong with the installation',
+  //     type: 'error',
+  //     heightAuto: false,
+  //   })
+  // }
 }
--- src/renderer.js
+++ src/renderer.js
@@ -5,12 +5,32 @@ require('materialize-css')
 // require("materialize-css/dist/css/materialize.min.css");
 
 document.addEventListener('DOMContentLoaded', e => {
-  let startBtn = document.querySelector('.start-btn')
+  let startBtn = document.querySelector('.automation-station button')
+  let queryTextArea = document.querySelector('.automation-station textarea')
+  let instancesInput = document.querySelector('.automation-station input')
 
   startBtn.addEventListener('click', async e => {
     console.log('start it')
+    let searchPhrases = queryTextArea.value.split(/\r?\n/)
+    let instances = instancesInput.value
+
+    if (instances <= 0 || instances > 5)
+      return Swal2.fire({
+        type: 'error',
+        title: 'Invalid Automation Instances',
+        text:
+          'Please enter the amount of browser you want to run which can be 1-5',
+        heightAuto: false,
+      })
+
+    // if the amount of instances is greater than the number of searches to run
+    // set instances down to those amount of searches
+    if (instances > searchPhrases.length) {
+      instances = searchPhrases.length
+    }
+
     try {
-      await runAutomation()
+      await runAutomation(searchPhrases, instances)
     } catch (err) {
       console.error('Err occured')
 
@@ -19,7 +39,7 @@ document.addEventListener('DOMContentLoaded', e => {
       Swal2.fire({
         title: 'Error',
         text:
-          'An error occured running the automation.  Perhaps something went wrong with the installation',
+          'An error occured running the automation.  Perhaps something went wrong with the installation.  Try deleting and reinstalling node_modules and try again',
         type: 'error',
         heightAuto: false,
       })
